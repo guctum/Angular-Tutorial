cacc61d745810a195f91c64416dd8237afcae4ea
Greg Uctum <uctumg@mail.gvsu.edu>
2018-06-01 18:00:53 -0400

    Completed tutorial
    Initial AngularJS tutorial completed

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,19 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr. Diabolical' },
+      { id: 12, name: 'The Masked Roberto' },
+      { id: 13, name: 'Yellow Lightbulb Man' },
+      { id: 14, name: 'Inertiaman' },
+      { id: 15, name: 'Vanilla Chinchilla' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Skunkman' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magmanaic' },
+      { id: 20, name: 'Tornado of Justice' }
+    ];
+    return {heroes};
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Diabolical' },
+  { id: 12, name: 'The Masked Roberto' },
+  { id: 13, name: 'Yellow Lightbulb Man' },
+  { id: 14, name: 'Inertiaman' },
+  { id: 15, name: 'Vanilla Chinchilla' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Skunkman' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magmanaic' },
+  { id: 20, name: 'Tornado of Justice' }
+];
